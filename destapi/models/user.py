9f5c4181a1d2b8b149dab0c2c0841c80ba80eb45
Eli Bradford <elibradford227@gmail.com>
2024-01-29 19:26:22 -0600

    g

--- destapi/models/user.py
+++ destapi/models/user.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    firebaseKey = models.CharField(max_length=50)
+    uid = models.CharField(max_length=50)
