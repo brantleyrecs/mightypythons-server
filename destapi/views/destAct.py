af908020f59b11a7310358ba97f8b204b856f793
Eli Bradford <elibradford227@gmail.com>
2024-01-29 21:00:47 -0600

    destact view made

---
+++ destapi/views/destAct.py
@@ -0,0 +1,83 @@
+from django.http import HttpResponseServerError
+from django.db.models import Count
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from destapi.models import DestAct, Destination, Activity
+
+class DestActView(ViewSet):
+    """Level up destAct view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single destination activitys
+
+        Returns:
+            Response -- JSON serialized destination activity
+        """
+        destAct = DestAct.objects.get(pk=pk)
+        serializer = DestActSerializer(destAct, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def list(self, request):
+        """Handle GET requests to get all destination activitys
+
+        Returns:
+            Response -- JSON serialized list of destination activitys
+        """
+        destAct = DestAct.objects.all()
+        serializer = DestActSerializer(destAct, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+      
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+          Response -- JSON serialized destination activity instance
+        """
+        destination = Destination.objects.get(pk=request.data['destination'])
+        activity = Activity.objects.get(pk=request.data['activity'])
+        destAct = DestAct.objects.create(
+            destination = destination,
+            activity = activity
+        )
+
+        serializer = DestActSerializer(destAct, many=False)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a destination activity
+
+        Returns:
+          Response -- Empty body with 204 status code
+        """
+
+        destAct = DestAct.objects.get(pk=pk)
+      
+        destAct.destination = request.data["destination"],
+        destAct.activity = request.data["activity"]
+
+        destAct.save()
+        
+        serializer = DestActSerializer(destAct, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def destroy(self, response, pk):
+        """Deletes Data
+
+        Returns:
+            Response: Empty body with 204 code
+        """
+        destAct = DestAct.objects.get(pk=pk)
+        destAct.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+        
+
+class DestActSerializer(serializers.ModelSerializer):
+    """JSON serializer for destination activitys
+
+    """
+    class Meta:
+        model = DestAct
+        fields = ('id', 'destination', 'activity')
+        depth = 1
