be96bf9ec3170cce8de8c5eeedf16f8442394417
Eli Bradford <elibradford227@gmail.com>
2024-02-01 19:45:38 -0600

    climate tests working

---
+++ dest/tests/utils.py
@@ -0,0 +1,62 @@
+import random
+
+from faker import Faker
+
+from destapi.models import Destination, Activity, DestAct, User, Climate
+
+
+def create_data(cls):
+    cls.faker = Faker()
+    cls.users = []
+    cls.destinations = []
+    cls.activities = []
+    cls.climates = []
+    cls.destacts = []
+
+    for _ in range(0, 10):
+        user = User.objects.create(
+            name=cls.faker.name(),
+            uid=cls.faker.name()
+        )
+        cls.users.append(user)
+
+        activity = Activity.objects.create(
+            name=cls.faker.name(),
+            bio=cls.faker.name()
+        )
+        cls.activities.append(activity)
+
+        for _ in range(1, 10):
+            climate = Climate.objects.create(
+                name=cls.faker.name()
+            )
+            cls.climates.append(climate)
+            
+
+            destination = Destination.objects.create(
+                name=cls.faker.name(),
+                bio=cls.faker.sentence(),
+                image=cls.faker.url(),
+                climate=climate,
+                user=user
+            )
+            cls.destinations.append(destination)
+
+            destact = DestAct.objects.create(
+                destination=destination,
+                activity=activity,
+            )
+            cls.destacts.append(destact)
+
+
+def refresh_data(self):
+    for user in self.users:
+        user.refresh_from_db()
+    for climate in self.climates:
+        climate.refresh_from_db()
+    for activity in self.activities:
+        activity.refresh_from_db()
+    for destination in self.destinations:
+        destination.refresh_from_db()
+    for destact in self.destacts:
+        destact.refresh_from_db()
--- destapi/models/destination.py
+++ destapi/models/destination.py
@@ -6,5 +6,5 @@ class Destination(models.Model):
   name = models.CharField(max_length=50)
   bio = models.CharField(max_length=500)
   image=models.CharField(max_length=50)
-  climate=models.ForeignKey(Climate, on_delete=models.SET_DEFAULT, default=0, related_name='climate')
-  user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=0, related_name='user')
+  climate=models.ForeignKey(Climate, on_delete=models.CASCADE, related_name='climate')
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
